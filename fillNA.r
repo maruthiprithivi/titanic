65f1035abf7efda04eb99eb260f6706658ec9cf3
visitor0 <visitor0@mail.com>
2013-07-07 17:15:15 +0800

    Included function to replace missing age values

--- fillNA.r
+++ fillNA.r
@@ -10,6 +10,18 @@ fill = function(X, test, column)
 	test[na.i, column] = predict(regRF, rows)
 }
 
+# replace missing age values
+ageNA <- function(x)
+{
+  for (y in c("Master", "Miss", "Mr", "Mrs"))
+    x[is.na(x["age"]) & y == x["title"], ]["age"] = 0.5 * (colMeans(x[y == x["title"], ]["age"], na.rm = T) + median(x[y == x["title"],"age"], na.rm = T))
+  if (sum(is.na(x["age"]) & "Dr" == x["title"]) > 0)
+    x[is.na(x["age"]) & "Dr" == x["title"], ]["age"] = 0.5 * (colMeans(x["Dr" == x["title"], ]["age"], na.rm = T) + median(x["Dr" == x["title"],"age"], na.rm = T))
+
+  return(x)
+
+}
+
 # fill missing test values
 # Xage = Xfill
 # Xage$age = NULL

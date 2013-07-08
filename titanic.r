f69454b5e5b77d2c88a275b62b3a5e0c0e7f4068
chungkhim <chungkhim@yahoo.com>
2013-07-08 17:00:11 +0800

    Create convergence function.

--- titanic.r
+++ titanic.r
@@ -3,7 +3,7 @@ source("processData.r")
 
 formatData = function(fileName)
 {
-	input = refineData(rawData(sub(".csv", "", fileName)))
+    input = refineData(rawData(sub(".csv", "", fileName)))
 
     # drop unnecessary columns
     input$lastName = NULL
@@ -14,32 +14,28 @@ formatData = function(fileName)
     input$cabinNumber = NULL
     input$cabin = NULL
 
-	return(input)
+    return(input)
+}
+
+# convergence curve
+convergence = function(X)
+{
+    n = nrow(X)
+    validSize = n / 3
+    shuffle = sample.int(n)[1:validSize]
+    X1 = X[-shuffle, 2:ncol(X)]
+    X2 = X[ shuffle, 2:ncol(X)]
+    y1 = X[-shuffle, 1]
+    y2 = X[ shuffle, 1]
+    RF = randomForest(X1, y1, X2, y2, ntree = 8000)
+    plot(RF$err.rate[, 'OOB'], col = "red")
+    lines(RF$test[["err.rate"]][, "Test"])
 }
 
 train = formatData("train.csv")
 test = formatData("test.csv")
 
-X = rfImpute(survived ~ ., train) # fill missing values
+# X = rfImpute(survived ~ ., train) # fill missing values
 # RF = randomForest(survived ~ ., X, ntree = 8000)
 # plot(RF$err.rate[, "OOB"])
 # savePlot("figures/OOB error vs Index", "png")
-
-# convergence curve
-# X1 = X[1:594, 2:ncol(X)]
-# y1 = X[1:594, 1]
-# X2 = X[595:891, 2:ncol(X)]
-# y2 = X[595:891, 1]
-# RF = randomForest(X1, y1, X2, y2, ntree = 8000)
-# plot(RF$err.rate[, 'OOB'])
-
-# missRF = randomForest(survived ~ ., train, ntree = 100, mtry = 3, na.action = na.omit)
-# fillRF = randomForest(survived ~ ., X, ntree = 100, mtry = 3)
-
-# p1 = predict(missRF, X0)
-# p2 = predict(fillRF, X0)
-
-# write(as.vector(p1), "prediction1.csv", 1)
-# write(as.vector(p2), "prediction2.csv", 1)
-
-# write.csv(data.frame(PassengerId = 1:418, Survived = p) , "./prediction.csv", row.names = F)

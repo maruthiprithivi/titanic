31e3eb4c9a5c253126dc6b08f8e3f8fd70e911b4
visitor0 <visitor0@mail.com>
2013-07-08 18:05:54 +0800

    Included a function to create a matrix of predictions to look at error prediction

--- predict.r
+++ predict.r
@@ -39,3 +39,19 @@ predictRF = function(train, test, ...)
 p = predictRF(train, test)
 write.csv(data.frame(PassengerId = 1:nrow(test), Survived = p),
           "prediction.csv", row.names = F)
+
+# create a matrix of predictions to look at error prediction
+# x is the training set, y is the test set and n is the number of predictions
+preMat <- function(x, y, n)
+{
+    pM = matrix(nrow = n, ncol = nrow(y))
+    for (i in 1 : n)
+    {
+        xRf = classifier(x)
+        p = predict(xRf, y)
+        pM[i, ] = as.integer(levels(p))[p]
+    }
+
+    return(pM)
+
+}
